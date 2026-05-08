efff083b7d2383ce515c90dfce2ff3a371031906
AlanCosta2102 <costa.alan@escolar.ifrn.edu.br>
2026-05-08 16:17:11 -0300

    Adicionando limite de assentos por veiculo

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,5 +1,8 @@
 from ..models import Reservation,Ride,Vehicle,UserClient
 from rest_framework import serializers
+from django.core.exceptions import ValidationError
+from ..models import Reservation, Ride, Vehicle, UserClient
+
 
 class UserClientSerializer(serializers.ModelSerializer):
     class Meta:
@@ -15,9 +18,22 @@ class ReservationSerializer(serializers.ModelSerializer):
 class RideSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ride
-        fields = ['vehicle', 'origin', 'destination', 'start_time', 'end_time','seats', 'status', 'price']
+        fields = ['vehicle', 'origin', 'destination', 'start_time', 'end_time','expected_arrival','available_seats', 'status', 'price']
+    
+    def validate(self,data):
+        vehicle = data['vehicle']
+        available_seats = data['available_seats']
+
+        if available_seats > vehicle.seats:
+            raise serializers.ValidationError(
+                {
+                    "available_seats":
+                    f"O veículo possui apenas {vehicle.seats} assentos."
+                }
+            )
+        return data
 
 class VehicleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Vehicle
-        fields = ['user', 'model', 'color', 'plate']
+        fields = ['user', 'model', 'color', 'plate','seats']
