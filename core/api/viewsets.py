29420aaeb623d5d2f58bb857fa3aada93352a76d
FabricioS2 <fabricio.g@escolar.ifrn.edu.br>
2026-06-15 23:07:22 -0300

    implementando filtros

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -1,14 +1,17 @@
 from rest_framework.viewsets import ModelViewSet
 from ..models import Reservation,Ride,Vehicle,Rating,UserClient
 from .serializers import ReservationSerializer,RideSerializer,VehicleSerializer, RatingSerializer, UserClientSerializer
+from .filters import RideFilter   # ajuste o import conforme sua estrutura
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
 
 class ReservationViewset(ModelViewSet):
     queryset = Reservation.objects.all()
     serializer_class = ReservationSerializer
 
-class RideViewset(ModelViewSet):
-    queryset = Ride.objects.all()
-    serializer_class = RideSerializer
+# class RideViewset(ModelViewSet):
+#     queryset = Ride.objects.all()
+#     serializer_class = RideSerializer
 
 class VehicleViewset(ModelViewSet):
     queryset = Vehicle.objects.all()
@@ -35,4 +38,11 @@ class UserClientViewset(ModelViewSet):
     queryset = UserClient.objects.all()
     serializer_class = UserClientSerializer
 
-    
+class RideViewset(ModelViewSet):
+    queryset = Ride.objects.all()
+    serializer_class = RideSerializer
+    filterset_class = RideFilter
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    search_fields = ['origin', 'destination', 'vehicle__model']   # campos onde fará busca textual
+    ordering_fields = ['price', 'start_time', 'available_seats']   # campos permitidos para ordenar
+    ordering = ['start_time']                                      # ordenação padrão (opcional)
