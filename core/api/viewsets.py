9d8252b76bc26a05bbdd4a8c109909b41c119a9a
AlanCosta2102 <costa.alan@escolar.ifrn.edu.br>
2026-06-16 10:55:14 -0300

    feat: Implementação de permissões de usuários.

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -1,16 +1,26 @@
 from rest_framework.viewsets import ModelViewSet
 from ..models import Reservation,Ride,Vehicle,Rating,UserClient
 from .serializers import ReservationSerializer,RideSerializer,VehicleSerializer, RatingSerializer, UserClientSerializer
+from .permissions import IsDriver
 
 class ReservationViewset(ModelViewSet):
+    
+    permission_classes = [IsDriver]
+    
     queryset = Reservation.objects.all()
     serializer_class = ReservationSerializer
 
 class RideViewset(ModelViewSet):
+
+    permission_classes = [IsDriver]
+    
     queryset = Ride.objects.all()
     serializer_class = RideSerializer
 
 class VehicleViewset(ModelViewSet):
+
+    permission_classes = [IsDriver]
+
     queryset = Vehicle.objects.all()
     serializer_class = VehicleSerializer
 
