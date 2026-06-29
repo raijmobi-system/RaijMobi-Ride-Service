dc0658bd2904805462944a0e3f226f873b58ee95
AlanCosta2102 <costa.alan@escolar.ifrn.edu.br>
2026-06-29 12:12:55 -0300

    feat: Implementação de metricas do prometheus para IA.

--- core/metrics.py
+++ core/metrics.py
@@ -1,4 +1,4 @@
-from prometheus_client import Counter
+from prometheus_client import Counter,Histogram
 
 rides_total = Counter(
     "rides_total",
@@ -13,4 +13,29 @@ reservations_total = Counter(
 cancelations_total = Counter(
     "cancelations_total",
     "Total de cancelamentos"
+)
+
+ai_requests_total = Counter(
+    "ai_requests_total",
+    "Total de chamadas realizadas para a IA"
+)
+
+ai_requests_failed_total = Counter(
+    "ai_requests_failed_total",
+    "Total de chamadas para IA que falharam"
+)
+
+ai_recommendations_total = Counter(
+    "ai_recommendations_total",
+    "Total de recomendações geradas pela IA"
+)
+
+ai_filters_total = Counter(
+    "ai_filters_total",
+    "Total de filtros inteligentes realizados"
+)
+
+ai_response_time_seconds = Histogram(
+    "ai_response_time_seconds",
+    "Tempo de resposta da IA"
 )
